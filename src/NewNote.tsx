198de7da35b411e6be00599b32d7a89e77f2f6d3
RaevenV <villarelr@gmail.com>
2024-09-24 08:27:36 +0700

    first

---
+++ src/NewNote.tsx
@@ -0,0 +1,22 @@
+import { NoteData, Tag } from "./App";
+import { NoteForm } from "./NoteForm";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
+  return (
+    <>
+      <h1 className="mt-2">New Note</h1>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </>
+  );
+}
